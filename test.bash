95723854e2531640056cd2b844df96071e419d78
Riku Kinjo <sansbook@icoud.com>
2024-12-13 10:58:16 +0900

    test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 # SPDX-FileCopyrightText: 2024 Riku Kinjo
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -10,16 +10,25 @@ ng () {
 res=0
 
 
-#正常な変換
-out=$(echo "1 c m"| ./rrtask1)
+#正常な変換(センチからミリ)
+out=$(echo "1 c m"| ./prefix_conversion)
 [ "${out}" = "10.0 m" ] || ng "$LINENO"
 
-#文字
-out=$(echo "A c m"| ./rrtask1)
+
+#文字を入力した場合
+out=$(echo "A c m"| ./prefix_conversion)
 [ "$?" = 1 ]            || ng "$LINENO"
 [ "${out}" = "" ]       || ng "$LINENO"
 
+#入力内容が不足した場合
+out=$(echo "10"| ./prefix_conversion)
+[ "$?" = 1 ]            || ng "$LINENO"
+[ "${out}" = "" ]       || ng "$LINENO"
 
+#空白が無い場合
+out=$(echo "1cm"| ./prefix_conversion)
+[ "$?" = 1 ]            || ng "$LINENO"
+[ "${out}" = "" ]       || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo no problem
 exit $res
